edf528f808532f7e4c6014a09ac8fffca732b669
Vincent Koopmans <vincent.koopmans@gmail.com>
2025-11-04 16:45:11 +0100

    Version 1.3: Standardize tooltip icons to blue info icon

--- components/DetailedResults.tsx
+++ components/DetailedResults.tsx
@@ -148,8 +148,8 @@ export default function DetailedResults({ data, inputs }: DetailedResultsProps) 
                         <div className="flex items-center gap-2">
                           <span className="text-gray-600">• Pensioen ({pensionBasePct}% × {pensionTotalPct}%, max 30% jaarruimte):</span>
                           <div className="group relative">
-                            <svg className={`w-4 h-4 cursor-help ${Math.abs(pensionTotalPct - (pensionEmployee + wgPensionEmployer)) > 1 ? 'text-amber-500' : 'text-gray-400'}`} fill="currentColor" viewBox="0 0 20 20">
-                              <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                            <svg className="w-4 h-4 cursor-help text-blue-500" fill="currentColor" viewBox="0 0 20 20">
+                              <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
                             </svg>
                             <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-72 p-2 bg-gray-800 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none z-10 whitespace-normal">
                               {Math.abs(pensionTotalPct - (pensionEmployee + wgPensionEmployer)) > 1 && (
